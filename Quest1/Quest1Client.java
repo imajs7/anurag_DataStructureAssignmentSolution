3e42550d6b3eef4364422881533ecf847c837230
imajs7 <anukhg@gmail.com>
2022-04-25 13:52:37 +0530

    blank project files : initial commit

---
+++ Quest1/Quest1Client.java
@@ -0,0 +1,10 @@
+package Quest1;
+
+public class Quest1Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Quest2/Quest2Client.java
@@ -0,0 +1,10 @@
+package Quest2;
+
+public class Quest2Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
